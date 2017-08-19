36e31acf7c3e1a085cb053cb0fa61d6a1c22bf2d
Tony Thomas <tonythomas22@protonmail.com>
2017-08-19 16:43:20 -0700

    fixed gulpfile sass bug

--- gulpfile.js
+++ gulpfile.js
@@ -28,13 +28,7 @@ gulp.task('serve', ['sass'], function(){
     browserSync.init({
         server: './src'
     });
-
-    gulp.watch([
-        'node_modules/bootstrap/scss/bootstrap.scss',
-        'src/scss/*.scss',
-        ['sass']
-    ]);
-
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
